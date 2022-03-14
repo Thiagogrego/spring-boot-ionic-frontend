89167e07556fed8604380f2497cd532b1ce04263
Grego <thiago.t.grego@accenture.com>
2022-03-14 16:30:19 -0300

    Tela de escolha de pagamento

--- src/models/pedido.dto.ts
+++ src/models/pedido.dto.ts
@@ -3,6 +3,7 @@ import { PagamentoDTO } from "./pagamento.dto";
 import { ItemPedidoDTO } from "./item-pedido.dto";
 
 
+
 export interface PedidoDTO {
     cliente: RefDTO;
     enderecoDeEntrega: RefDTO;
