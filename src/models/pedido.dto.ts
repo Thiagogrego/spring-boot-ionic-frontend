03dc7a55a9a01b5c1fa38057e2ff56c918575364
Grego <thiago.t.grego@accenture.com>
2022-03-16 09:49:23 -0300

    Tela de confirmacao de pedido

--- src/models/pedido.dto.ts
+++ src/models/pedido.dto.ts
@@ -2,8 +2,6 @@ import { RefDTO } from "./ref.dto";
 import { PagamentoDTO } from "./pagamento.dto";
 import { ItemPedidoDTO } from "./item-pedido.dto";
 
-
-
 export interface PedidoDTO {
     cliente: RefDTO;
     enderecoDeEntrega: RefDTO;
